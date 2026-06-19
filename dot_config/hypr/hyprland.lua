9cae8593e95bbedadb63a498a01c2b5fd9530858
p-lorenzo <p-lorenzo@local>
2026-06-19 12:00:27 +0200

    Configurato layout di input us(intl) per abilitare i tasti morti

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -232,7 +232,7 @@ hl.config({
 hl.config({
     input = {
         kb_layout  = "us",
-        kb_variant = "",
+        kb_variant = "intl",
         kb_model   = "",
         kb_options = "",
         kb_rules   = "",
