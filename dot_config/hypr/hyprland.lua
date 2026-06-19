fde71aa36408cb2b6abaca6f9ac191a40c519400
p-lorenzo <p-lorenzo@local>
2026-06-19 11:51:06 +0200

    Corretto orientamento monitor (DP-2 a sinistra, DP-1 a destra)

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -23,9 +23,16 @@
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-    output   = "",
+    output   = "DP-2",
     mode     = "preferred",
-    position = "auto",
+    position = "0x0",
+    scale    = "auto",
+})
+
+hl.monitor({
+    output   = "DP-1",
+    mode     = "preferred",
+    position = "1920x0",
     scale    = "auto",
 })
 
